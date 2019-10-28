a9c380ef9f8d487452cceb611c093044eb478ddc
Lương Kiều Thiên <41820342+ThienLuong95@users.noreply.github.com>
2019-10-28 09:00:21 +0700

    update

--- src/main/java/component/Room.java
+++ src/main/java/component/Room.java
@@ -8,69 +8,91 @@ import com.jogamp.opengl.util.texture.TextureIO;
 import java.io.File;
 
 public class Room implements IDisplayable {
-    private int texture;
+    private int textureFront, textureBack, textureBottom, textureTop, textureRight, textureLeft;
+    private float xrot,yrot,zrot;
     private GLUT glut = new GLUT();
+    private float witdh =10;
     public Room( ) {
 
     }
     public void Init(GL2 gl) {
-
+        textureFront = LoadTexture("front.png", gl);
+        textureBack = LoadTexture("back.png", gl);
+        textureTop = LoadTexture("top.png", gl);
+        textureBottom = LoadTexture("bottom.png", gl);
+        textureLeft = LoadTexture("left.png", gl);
+        textureRight = LoadTexture("right.png", gl);
+    }
+    private int LoadTexture(String name, GL2 gl){
+        Texture t = null;
         try
         {
-            File im = new File("src/main/resources/demo.jpg");
-            Texture t = TextureIO.newTexture(im, true);
-            texture= t.getTextureObject(gl);
-//            System.out.println("Texture: "+t);
+            StringBuilder path =new StringBuilder("src/main/resources/");
+            path.append(name);
+            File im = new File(path.toString());
+            t = TextureIO.newTexture(im, true);
         }
         catch (Exception e) {
 //            System.out.println("failure");
             System.out.println(e);
-        }
 
+        }
+        return t.getTextureObject(gl);
     }
     @Override
     public void Display(GL2 gl) {
         gl.glTranslated(0,0,0);
+
+        gl.glRotatef(xrot, 1.0f, 1.0f, 1.0f);
+        gl.glRotatef(yrot, 0.0f, 1.0f, 0.0f);
+        gl.glRotatef(zrot, 0.0f, 0.0f, 1.0f);
+
         gl.glEnable(GL2.GL_TEXTURE_2D);
-        gl.glBindTexture(GL2.GL_TEXTURE_2D, texture);
         gl.glBegin(GL2.GL_QUADS);
-
         // Front Face
-        gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f(-10.0f, -10.0f, 10.0f);
-        gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f( 10.0f, -10.0f, 10.0f);
-        gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f( 10.0f, 10.0f, 10.0f);
-        gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f(-10.0f, 10.0f, 10.0f);
-
+        gl.glBindTexture(GL2.GL_TEXTURE_2D, textureFront);
+        gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f(-witdh, -witdh, witdh);
+        gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f( witdh, -witdh, witdh);
+        gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f( witdh, witdh, witdh);
+        gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f(-witdh, witdh, witdh);
         // Back Face
-        gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f(-10.0f, -10.0f, -10.0f);
-        gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f(-10.0f, 10.0f, -10.0f);
-        gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f( 10.0f, 10.0f, -10.0f);
-        gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f( 1.0f, -1.0f, -10.0f);
+        gl.glBindTexture(GL2.GL_TEXTURE_2D, textureBack);
+        gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f(-witdh, -witdh, -witdh);
+        gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f(-witdh, witdh, -witdh);
+        gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f( witdh, witdh, -witdh);
+        gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f( witdh, -witdh, -witdh);
 
         // Top Face
-        gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f(-10.0f, 10.0f, -10.0f);
-        gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f(-10.0f, 10.0f, 10.0f);
-        gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f( 10.0f, 10.0f, 10.0f);
-        gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f( 10.0f, 10.0f, -1.00f);
+        gl.glBindTexture(GL2.GL_TEXTURE_2D, textureTop);
+        gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f(-witdh, witdh, -witdh);
+        gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f(-witdh, witdh, witdh);
+        gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f( witdh, witdh, witdh);
+        gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f( witdh, witdh, -witdh);
 
         // Bottom Face
-        gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f(-10.0f, -10.0f, -10.0f);
-        gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f( 10.0f, -10.0f, -10.0f);
-        gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f( 10.0f, -10.0f, 10.0f);
-        gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f(-10.0f, -10.0f, 10.0f);
+        gl.glBindTexture(GL2.GL_TEXTURE_2D, textureFront);
+        gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f(-witdh, -witdh, -witdh);
+        gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f( witdh, -witdh, -witdh);
+        gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f( witdh, -witdh, witdh);
+        gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f(-witdh, -witdh, witdh);
 
         // Right face
-        gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f( 10.0f, -10.0f, -10.0f);
-        gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f( 10.0f, 10.0f, -10.0f);
-        gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f( 10.0f, 10.0f, 10.0f);
-        gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f( 10.0f, -10.0f, 10.0f);
+        gl.glBindTexture(GL2.GL_TEXTURE_2D, textureRight);
+        gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f( witdh, -witdh, -witdh);
+        gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f( witdh, witdh, -witdh);
+        gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f( witdh, witdh, witdh);
+        gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f( witdh, -witdh, witdh);
 
         // Left Face
-        gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f(-10.0f, -10.0f, -10.0f);
-        gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f(-10.0f, -10.0f, 10.0f);
-        gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f(-10.0f, 10.0f, 10.0f);
-        gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f(-10.0f, 10.0f, -10.0f);
+        gl.glBindTexture(GL2.GL_TEXTURE_2D, textureLeft);
+        gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f(-witdh, -witdh, -witdh);
+        gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f(-witdh, -witdh, witdh);
+        gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f(-witdh, witdh, witdh);
+        gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f(-witdh, witdh, -witdh);
         gl.glEnd();
+//        xrot += .3f;
+        yrot += .3f;
+//        zrot += .3f;
 
         gl.glDisable(GL2.GL_TEXTURE_2D);
     }
--- src/main/java/ui/DisplayManager.java
+++ src/main/java/ui/DisplayManager.java
@@ -24,7 +24,7 @@ public class DisplayManager implements GLEventListener, KeyListener {
         private Robot robot;
         private Room room;
 
-        private float ex=0,ey=0,ez=2;
+        private float ex=0,ey=0,ez=20;
         public DisplayManager() {
             glu =  new GLU();
             grid = new Grid(new Point3D(0f,0f, 0f), lineCount);
